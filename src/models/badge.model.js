1c16356e35d615d9d198116a08f91467f15b1b8e
Cesar Velez <cavb.28@gmail.com>
2020-03-05 15:09:34 -0500

    badge added

---
+++ src/models/badge.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const badgeSchema = new Schema({
+  name:
+      {
+        type: String,
+        required: true,
+        unique: true
+      },
+  logo:
+      {
+        type: Schema.Types.ObjectID,
+        required: true
+      },
+  superadmin:
+      {
+        type: String,
+        ref: 'user',
+        required: false,
+        autopopulate: false
+      },
+  admins:
+      [{
+        type: Schema.Types.ObjectID
+      }],
+  drivers:
+        [{
+          type: Schema.Types.ObjectID
+        }],
+  vehicles:
+        [{
+          type: Schema.Types.ObjectID
+        }]
+},
+{
+  timestamps: true
+}
+);
+badgeSchema.plugin(require('mongoose-autopopulate'));
+module.exports = mongoose.model('badge', badgeSchema);
