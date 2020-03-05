82e5302f4d61946eda7438f8489388d8f84fd61c
svelezg <svelez.velezgarcia@gmail.com>
2020-03-05 14:58:44 -0500

    user and ratin models

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,23 +2,67 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const {compareSync, hashSync, genSaltSync} = require('bcryptjs');
 
-const userSchema = new Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true}
-})
+const userSchema = new Schema(
+    {
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true
+        },
+    password: {
+        type: String, 
+        required: true
+        },
+    type: {
+        type: String,
+        required: true,
+        enum: ['normal', 'driver', 'admin'],
+        default: 'normal'
+        },
+    name: {
+        type: String,
+        trim: true
+    },
+    photo: {
+        type: String
+    }, 
+    twitterUser: {
+        type: String,
+    },
+    vehicleId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: function() { return this.type === 'driver'; }
 
-userSchema.methods.checkPassword = function(password) {
-    return compareSync(password, this.password);
-}
+    },
+    badgeId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: function() { return this.type === 'admin'; }
+    }
+
+},
+    { timestamps: true }    
+)
+
 userSchema.pre('save', async function(next) {
     const user = this;
-    if(!user.isModified('password')) {
+    try {
+       if(!user.isModified('password')) {
         return next();
     }
     const salt = genSaltSync(10);
     const hashedPassword = hashSync(user.password, salt);
     user.password = hashedPassword;
-    next();
-
+    next(); 
+    }
+    catch(err) {
+        console.log(err)
+        return next(err)
+    }
 })
+
+userSchema.methods.checkPassword = function(password) {
+    const passwordHash = this.password
+    return compareSync(password, passwordHash);
+}
 module.exports = mongoose.model('user', userSchema);
