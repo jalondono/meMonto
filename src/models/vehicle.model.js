70a564036321056bd6e668087773435fd7855e27
Cesar Velez <cavb.28@gmail.com>
2020-03-05 09:41:02 -0500

    created and updated in vehicles

--- src/models/vehicle.model.js
+++ src/models/vehicle.model.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const  {Schema} = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const vehicleSchema = new Schema({
-    license_plate: {type: String, required: true},
-    /*reviews: [{
+  license_plate: { type: String, required: true }
+  /* reviews: [{
         type: Schema.Types.ObjectId,
         ref: "reviews",
         require: true,
         autopopulate: true
     }]
     */
-});
+
+}, { timestamps: true });
 vehicleSchema.plugin(require('mongoose-autopopulate'));
 module.exports = mongoose.model('vehicle', vehicleSchema);
