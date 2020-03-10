403e89ec06ca38ad8d8420189723da92ab5a0cd2
svelezg <svelez.velezgarcia@gmail.com>
2020-03-10 10:21:02 -0500

    modified: server.js added cors corrected

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {BadgeRoute} = require('./src/routes');
 
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
